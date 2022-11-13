08e71ce8ededb385aa53cff0762d3b8cb46a7df2
Ruiyang Liu <rliu60@ucsc.edu>
2022-11-12 20:36:42 -0800

    first

---
+++ client.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <errno.h> 
+#include <string.h> 
+#include <netdb.h> 
+#include <sys/types.h> 
+#include <netinet/in.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <sys/wait.h>
+#include <iostream>
+#include <string>
+#include <signal.h>
+#include <sys/wait.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    
+}
